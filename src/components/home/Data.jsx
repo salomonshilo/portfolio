9abc81c2fba4672d36e3acd5aba9e4fb8be4e464
salomonshilo <shilomavonga@mail.com>
2022-09-30 15:24:42 +0100

    ajout des element acceuil

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,20 @@
+ import React from 'react'
+ 
+ function Data() {
+   return (
+     <div className='home_data'>
+      <h1 className='home_title'>  Mavonga shilo </h1>
+
+      <h3 className='home_subtitle'>Développeur fullstack</h3>
+      <p className='home_description'> Je suis developeur residant en RDC,  donner vie à vos projet est mon reve le plus fou.
+      </p>
+      <a href='#contact' className='button button--flex'>
+      
+        Dites bonjour
+        
+      </a>
+     </div>
+   )
+ }
+ 
+ export default Data
