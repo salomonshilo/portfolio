00d0a258db0cc14368e86e62c782f463fdea1135
salomonshilo <shilomavonga@mail.com>
2023-01-29 13:11:57 +0100

    feature: link to github

--- src/components/portfolio/Porfolio.jsx
+++ src/components/portfolio/Porfolio.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './porfolio.css'
 
+import Img1 from "../../assets/image2Pc.jpg";
+import img2 from "../../assets/imagePc.webp"
+
 const Porfolio = () => {
   return (
     <section className="porfolio section" id="porfolio">
@@ -8,51 +11,79 @@ const Porfolio = () => {
       <span className="section_subtitle">Ce que j'ai réalisé</span>
 
       <div className="contain_realisation">
-        {/* <img alt='' src='' className='image_realisation' /> */}
         <div className="my_realisation">
-          <div className="essaie"></div>
-          <span className="text_realisation">lorem lorem ipstum</span>
+          <img alt="" src={img2} className="image_realisation" />
+          <p>Clone spotify</p>
+          <span className=" services_button">
+            <a href="https://github.com/salomonshilo/"> Voir sur github</a>
+            <i
+              className="uil uil-arrow-right 
+            services_button-icon"
+            ></i>
+          </span>
         </div>
 
-        
-
         <div className="my_realisation">
-          <div className="essaie"></div>
-          <span className="text_realisation">lorem lorem ipstum</span>
+          <img alt="" src={Img1} className="image_realisation" />
+          <p>chat App</p>
+          <span className="text_realisation services_button">
+            <a href="https://github.com/salomonshilo/"> Voir sur github</a>
+            <i
+              className="uil uil-arrow-right 
+            services_button-icon"
+            ></i>
+          </span>
         </div>
 
         <div className="my_realisation">
-          <div className="essaie"></div>
-          <span className="text_realisation">lorem lorem ipstum</span>
+          <img alt="" src={img2} className="image_realisation" />
+          <p>Landing page</p>
+          <span className="text_realisation services_button">
+            <a href="https://github.com/salomonshilo/"> Voir sur github</a>
+            <i
+              className="uil uil-arrow-right 
+            services_button-icon"
+            ></i>
+          </span>
         </div>
       </div>
-
-      
-
-
-
-
-
-
-
-
 
       <div className="contain_realisation">
         {/* <img alt='' src='' className='image_realisation' /> */}
         <div className="my_realisation">
-          <div className="essaie"></div>
-          <span className="text_realisation">lorem lorem ipstum</span>
+          <img alt="" src={Img1} className="image_realisation" />
+          <p>Quiz App</p>
+          <span className="text_realisation services_button">
+            <a href="https://github.com/salomonshilo/"> Voir sur github</a>
+            <i
+              className="uil uil-arrow-right 
+            services_button-icon"
+            ></i>
+          </span>
         </div>
 
         <div className="my_realisation">
-          <div className="essaie"></div>
-          <span className="text_realisation">lorem lorem ipstum</span>
+          <img alt="" src={img2} className="image_realisation" />
+          <p>Responsive porfolio</p>
+          <span className="text_realisation services_button">
+            <a href="https://github.com/salomonshilo/"> Voir sur github</a>
+            <i
+              className="uil uil-arrow-right 
+            services_button-icon"
+            ></i>
+          </span>
         </div>
 
-       
         <div className="my_realisation">
-          <div className="essaie"></div>
-          <span className="text_realisation">lorem lorem ipstum</span>
+          <img alt="" src={Img1} className="image_realisation" />
+          <p>Site maison jungle</p>
+          <span className="text_realisation services_button">
+            <a href="https://github.com/salomonshilo/"> Voir sur github</a>
+            <i
+              className="uil uil-arrow-right 
+            services_button-icon"
+            ></i>
+          </span>
         </div>
       </div>
     </section>
